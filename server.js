803d64256b6861da649fd708031a5552e213ccab
Paremai <pramai12hong@gmail.com>
2026-04-15 13:34:32 +0700

    fix server.js

--- server.js
+++ server.js
@@ -80,3 +80,9 @@ app.get("/users", (req, res) => {
 app.listen(3000, () => {
     console.log("http://localhost:3000");
 });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running");
+});
